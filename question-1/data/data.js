f1b767d7b76d1608f8b766237aac37caf6104e9e
Damitha Perera <damithaperera777@gmail.com>
2023-05-17 19:01:16 +0530

    files

---
+++ question-1/data/data.js
@@ -0,0 +1,22 @@
+const residents = [
+    {
+        id: 1,
+        name: 'John Doe',
+        biography: {
+            age: 30,
+            occupation: 'Engineer',
+            address: '123 Main St',
+        },
+        relatives: ['Jane Doe', 'James Doe'],
+    },
+    {
+        id: 2,
+        name: 'Mack Doe',
+        biography: {
+            age: 20,
+            occupation: 'Doctor',
+            address: '223 Main St',
+        },
+        relatives: ['Jane Doe', 'James Doe'],
+    },
+];
